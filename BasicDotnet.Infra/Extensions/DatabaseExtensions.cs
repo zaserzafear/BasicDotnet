ddc8bcebd584d3ac6291683e300c8bc57a1fdbb1
Ratchanon Unnu <ratchanon.u@smartmedtech.co.th>
2025-03-04 14:55:11 +0700

    feat: connected MySql

---
+++ BasicDotnet.Infra/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,15 @@
+﻿using BasicDotnet.Infra.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BasicDotnet.Infra.Extensions;
+
+public static class DatabaseExtensions
+{
+    public static void ApplyMigrations(this IServiceProvider services)
+    {
+        using var scope = services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        dbContext.Database.Migrate();
+    }
+}
--- BasicDotnet.WebApi/Program.cs
+++ BasicDotnet.WebApi/Program.cs
@@ -1,41 +1,43 @@
 using BasicDotnet.App.Extensions;
 using BasicDotnet.Infra.Extensions;
 
-namespace BasicDotnet.WebApi
+namespace BasicDotnet.WebApi;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var configuration = builder.Configuration;
+
+        builder.Services.AddApplicationExtension(configuration);
+        builder.Services.AddInfrastructureExtension(configuration);
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
+            using var scope = app.Services.CreateScope();
+            scope.ServiceProvider.ApplyMigrations();
 
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var configuration = builder.Configuration;
-
-            builder.Services.AddApplicationExtension(configuration);
-            builder.Services.AddInfrastructureExtension(configuration);
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
