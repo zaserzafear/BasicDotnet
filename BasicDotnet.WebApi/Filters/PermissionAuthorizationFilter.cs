0ff59cb1389b91db1cb583f676e9977c760261d9
Ratchanon Unnu <ratchanon.u@smartmedtech.co.th>
2025-03-11 14:11:55 +0700

    feat: separate filter

--- BasicDotnet.WebApi/Filters/PermissionAuthorizationFilter.cs
+++ BasicDotnet.WebApi/Filters/PermissionAuthorizationFilter.cs
@@ -27,64 +27,34 @@ public class PermissionAuthorizationFilter : IAsyncAuthorizationFilter
             .OfType<HasPermissionAttribute>()
             .ToList();
 
-        var ownUserIdPermissionAttributes = endpoint?
-            .Metadata
-            .OfType<HasOwnUserIdPermissionAttribute>()
-            .ToList();
-
-        // If there are no permission attributes, continue with no permission check
-        if (permissionAttributes!.Count == 0 && ownUserIdPermissionAttributes!.Count == 0)
+        if (permissionAttributes is null || permissionAttributes.Count == 0)
         {
             return;
         }
 
-        string RequestId = _httpContextAccessor!.HttpContext!.TraceIdentifier;
+        string requestId = _httpContextAccessor.HttpContext?.TraceIdentifier ?? string.Empty;
 
-        // Fetch role claim from user
         var roleClaim = _httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.Role)?.Value;
         if (string.IsNullOrEmpty(roleClaim))
         {
-            context.Result = ResponseHelper.Error(RequestId, "Forbidden: No role claim found", 403);
+            context.Result = ResponseHelper.Error(requestId, "Forbidden: No role claim found", 403);
             return;
         }
 
-        var currentUserId = _httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.Name)?.Value;
-        if (string.IsNullOrEmpty(currentUserId))
-        {
-            context.Result = ResponseHelper.Error(RequestId, "Forbidden: No unique_name claim found", 403);
-            return;
-        }
-
-        // Track if permission checks are successful
         bool hasPermission = false;
-
-        // Check generic permissions first
-        foreach (var permissionAttribute in permissionAttributes!)
+        foreach (var permissionAttribute in permissionAttributes)
         {
             hasPermission = await _permissionRepository.HasPermissionAsync(int.Parse(roleClaim), permissionAttribute.PermissionName);
 
             if (hasPermission)
             {
-                break; // If permission found, exit loop early
+                break;
             }
         }
 
         if (!hasPermission)
         {
-            context.Result = ResponseHelper.Error(RequestId, "Forbidden: User does not have permission", 403);
-            return;
-        }
-
-        foreach (var ownCustomerPermissionAttribute in ownUserIdPermissionAttributes!)
-        {
-            // Assuming the user_id is passed as part of the route
-            var userIdFromRoute = context.RouteData.Values["user_id"]?.ToString();
-
-            if (userIdFromRoute != currentUserId)
-            {
-                context.Result = ResponseHelper.Error(RequestId, "Access denied: You are not authorized.", 403);
-                return;
-            }
+            context.Result = ResponseHelper.Error(requestId, "Forbidden: User does not have permission", 403);
         }
     }
 }
--- BasicDotnet.WebApi/Program.cs
+++ BasicDotnet.WebApi/Program.cs
@@ -19,6 +19,7 @@ public class Program
         builder.Services.AddControllers(options =>
         {
             options.Filters.Add<PermissionAuthorizationFilter>();
+            options.Filters.Add<OwnUserIdAuthorizationFilter>();
         });
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
