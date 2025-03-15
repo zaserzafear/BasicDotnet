e6d628c8dee9889c9bac11f4df245f4fada8b3fa
Ratchanon Unnu <ratchanon.unnu1993@gmail.com>
2025-03-15 15:02:44 +0700

    feat: web mvc add yarp for proxy /api to webapi

--- BasicDotnet.WebMvc/Controllers/HomeController.cs
+++ BasicDotnet.WebMvc/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
-using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
 using BasicDotnet.WebMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace BasicDotnet.WebMvc.Controllers;
 
--- BasicDotnet.WebMvc/Program.cs
+++ BasicDotnet.WebMvc/Program.cs
@@ -3,6 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -10,6 +13,9 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+
+app.MapReverseProxy();
+
 app.UseStaticFiles();
 
 app.UseRouting();
